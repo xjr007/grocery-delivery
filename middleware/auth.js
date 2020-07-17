1d987f60200bacdabc6a2ddf2964a9a63fb5a01e
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-17 17:06:27 +0200

    token verification and authentication

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const token = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+	const authToken = req.header('x-auth-token');
+
+	if (!authToken) {
+		return res.status(401).json({ message: 'Authorisation denied' });
+	}
+
+	try {
+		const decoded = token.verify(authToken, config.get('SECRET_KEY'));
+
+		req.user = decoded.user;
+		next();
+	} catch (err) {
+		console.error(err.message);
+		res.status(401).json({ message: 'Authorisation denied. Token not valid' });
+	}
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,21 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const token = require('jsonwebtoken');
 const config = require('config');
-const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/auth
 // @desc   Get logged in user
 // @access  Private
 
-router.get('/', (req, res) => {
-	res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   POST api/auth
