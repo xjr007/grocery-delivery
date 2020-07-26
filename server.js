895d7bd38067ffa3f50d42b9ab06ba14c56776ff
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-26 21:58:36 +0200

    postbuild and server js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const conn = require('./config/db');
+const parth = require('path');
 
 const app = express();
 
@@ -9,7 +10,7 @@ conn();
 // Mmiddleware
 app.use(express.json({ extend: false }));
 
-app.get('/', (req, res) => res.json({ message: 'Account API' }));
+// app.get('/', (req, res) => res.json({ message: 'Account API' }));
 
 /* Routes */
 
@@ -17,6 +18,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (re, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
