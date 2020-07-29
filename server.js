0e931078ef40401ffca0865d10eaba48e9d84ba8
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-29 12:57:17 +0200

    commit all changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const conn = require('./config/db');
-const parth = require('path');
+const path = require('path');
 
 const app = express();
 
@@ -10,8 +10,6 @@ conn();
 // Mmiddleware
 app.use(express.json({ extend: false }));
 
-// app.get('/', (req, res) => res.json({ message: 'Account API' }));
-
 /* Routes */
 
 app.use('/api/auth', require('./routes/auth'));
@@ -20,9 +18,9 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static('./client/build'));
 
-	app.get('*', (re, res) => {
+	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
