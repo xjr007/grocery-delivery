cf66953e46b6cb4b30cd4da7a4b532c374776e7b
abaaqielbehardien@gmail.com <abaaqielbehardien@gmail.com>
2020-08-06 21:42:33 +0200

    line 31 of users.js 'message' to 'msg'

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.post(
 			let user = await User.findOne({ email });
 
 			if (user) {
-				return res.status(400).json({ message: 'User already exists' });
+				return res.status(400).json({ msg: 'User already exists' });
 			}
 
 			user = new User({
