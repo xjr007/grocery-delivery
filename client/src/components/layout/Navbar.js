897d4dcf431f93d87a9386f3ee7dd0c840f400b5
Aaqiel <abaaqielbehardien@gmail.com>
2020-08-17 11:12:49 +0200

    logo and account moved to left and right./ Removed outer Nav as well. Dropdown needs minor changes.Maybe look to changing entire dropdown.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,27 +11,51 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavbarComp = ({ auth: { isAuthenticated, loading }, logout }) => {
 	return (
-		<Nav className=' sticky-top nav-custom m-1'>
-			<Nav className='top-nav'>
-				<LinkContainer to={ROUTES.HOME}>
-					<Nav.Link className='d-flex flex-column'>
-						<div>
-							<span className='d-logo'>B</span>
-							<span className='d-logo'>U</span>
-							<span className='d-logo'>Y</span>
-							<span className='ampersamp'>&</span>
-							<span className='d-logo'>D</span>
-							<span className='d-logo'>A</span>
-							<span className='d-logo'>S</span>
-							<span className='d-logo'>H</span>
-						</div>
+		<Navbar
+			collapseOnSelect
+			className='sticky-top ml-auto nav-custom navbar-custom navbar-dark '
+			expand='lg'>
+			<LinkContainer to={ROUTES.HOME} className='m-logo'>
+				<Nav.Link className='d-flex flex-column'>
+					<div>
+						<span className='d-logo'>B</span>
+						<span className='d-logo'>U</span>
+						<span className='d-logo'>Y</span>
+						<span className='ampersamp'>&</span>
+						<span className='d-logo'>D</span>
+						<span className='d-logo'>A</span>
+						<span className='d-logo'>S</span>
+						<span className='d-logo'>H</span>
+					</div>
 
-						<span className='sub-logo'>...FROM THE COMFORT OF YOUR COUCH</span>
-					</Nav.Link>
-				</LinkContainer>
+					<span className='sub-logo'>...FROM THE COMFORT OF YOUR COUCH</span>
+				</Nav.Link>
+			</LinkContainer>
 
-				{isAuthenticated && !loading ? (
-					<NavDropdown title='Account' id='nav-dropdown' className='ml-auto mt-2'>
+			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+			<Navbar.Collapse id='responsive-navbar-nav  '>
+				<Nav className='navbar-links d-flex justify-content-center align-items-center ml-auto mr-auto'>
+					<LinkContainer to={ROUTES.HOME}>
+						<Nav.Link className='link-nav '>Home</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to={ROUTES.ABOUT}>
+						<Nav.Link className='link-nav'>About us</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to={ROUTES.SHOP}>
+						<Nav.Link className='link-nav'>Shop</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to={ROUTES.DELIVERYINFO}>
+						<Nav.Link className='link-nav'>Delivery Info</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to={ROUTES.CONTACT}>
+						<Nav.Link className='link-nav'>Contact</Nav.Link>
+					</LinkContainer>
+					<Dropdown.Divider />
+				</Nav>
+			</Navbar.Collapse>
+			{isAuthenticated && !loading ? (
+				<Nav>
+					<NavDropdown id='nav-dropdown-basic' title='Account' className='accounts ml-auto'>
 						<LinkContainer to={ROUTES.PROFILE}>
 							<NavDropdown.Item className='link'>Account info</NavDropdown.Item>
 						</LinkContainer>
@@ -40,39 +64,16 @@ const NavbarComp = ({ auth: { isAuthenticated, loading }, logout }) => {
 							Log out
 						</NavDropdown.Item>
 					</NavDropdown>
-				) : (
-					<Nav className='ml-auto m-3'>
-						{' '}
-						<LinkContainer to={ROUTES.LOGIN}>
-							<Nav.Link className='link '>Login </Nav.Link>
-						</LinkContainer>
-					</Nav>
-				)}
-			</Nav>
-			<Navbar collapseOnSelect className='ml-auto navbar-custom navbar-dark ' expand='lg'>
-				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-				<Navbar.Collapse id='responsive-navbar-nav '>
-					<Nav>
-						<LinkContainer to={ROUTES.HOME}>
-							<Nav.Link className='link-nav '>Home</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to={ROUTES.ABOUT}>
-							<Nav.Link className='link-nav'>About us</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to={ROUTES.SHOP}>
-							<Nav.Link className='link-nav'>Shop</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to={ROUTES.DELIVERYINFO}>
-							<Nav.Link className='link-nav'>Delivery Info</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to={ROUTES.CONTACT}>
-							<Nav.Link className='link-nav'>Contact</Nav.Link>
-						</LinkContainer>
-						<Dropdown.Divider />
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		</Nav>
+				</Nav>
+			) : (
+				<Nav className='ml-auto m-3'>
+					{' '}
+					<LinkContainer to={ROUTES.LOGIN}>
+						<Nav.Link className='link '>Login </Nav.Link>
+					</LinkContainer>
+				</Nav>
+			)}
+		</Navbar>
 	);
 };
 
